02b102ba92abae4100af3138f1e46254fec06615
Константин Потекаев <kostya.potekaev@gmail.com>
2022-07-11 16:28:41 +0300

    Выполнено 1 задание

---
+++ Lesson_4/Building.cs
@@ -0,0 +1,42 @@
+﻿namespace Lesson_4;
+
+public class Building
+{
+    //Все поля приватные
+    private long number;
+    private double _height;
+    private int _floor;
+    private int _aparts;
+    private int _entance;
+
+    public Building(double height, int floor, int aparts, int entance)
+    {
+        number = GenerateID.GenerateId();
+        _height = height;
+        _floor = floor;
+        _aparts = aparts;
+        _entance = entance;
+    }
+    
+    public (double,int, int, int)  GetInfo()
+    {
+        return (_height, _floor, _aparts, _entance);
+    }
+
+    public double CalculatingFloorHeight()
+    {
+
+        return (double)(_height / CalculatingTheNumberOfApartmentsInEntrance());
+    }
+
+    public int CalculatingTheNumberOfApartmentsInEntrance()
+    {
+        return _aparts / _entance;
+    }
+
+    public int CalculateNumberOfApartementOnFloor()
+    {
+        return CalculatingTheNumberOfApartmentsInEntrance() / _floor;
+    }
+}
+
--- Lesson_4/Program.cs
+++ Lesson_4/Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Lesson_4;
+
+var build_1 = new Building(100, 10, 33, 3);
+Console.WriteLine(build_1.CalculatingFloorHeight());
+/*var build_2 = new Building(20, 10, 3);
+Console.WriteLine(build_1.GetInfo());*/
