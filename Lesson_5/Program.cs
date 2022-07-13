f9b7de6488c6541b2d64c9be197a008bd2ab7699
Константин Потекаев <kostya.potekaev@gmail.com>
2022-07-13 10:52:27 +0300

    Определены операторы == и !=

--- Lesson_5/Program.cs
+++ Lesson_5/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Lesson_5;
+
+var a = new Rational_number(1, 2);
+var b = new Rational_number(1, 2);
+
+
+Console.WriteLine(a == b);
+Console.WriteLine(a != b);
+
---
+++ Lesson_5/Rational_numbers.cs
@@ -0,0 +1,21 @@
+﻿namespace Lesson_5;
+
+public class Rational_number
+{
+    public int _numerator, _denominator;
+    public Rational_number(int numerator, int denominator)
+    {
+        _numerator = numerator;
+        _denominator = denominator;
+    }
+
+    public static bool operator ==(Rational_number a, Rational_number b)
+    {
+        return Equals(a._numerator/a._denominator, b._numerator/b._denominator); 
+    }
+    public static bool operator !=(Rational_number a, Rational_number b)
+    {
+        return !Equals(a._numerator / a._denominator, b._numerator / b._denominator);
+    }
+
+}
