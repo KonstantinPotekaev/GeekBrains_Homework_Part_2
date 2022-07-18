f50081ec79c9ab3a8c2f0cca93eeb428893f3eaa
Константин Потекаев <kostya.potekaev@gmail.com>
2022-07-18 16:24:33 +0300

    Переопределен вывод и хэш

--- Lesson_2_3/Program.cs
+++ Lesson_2_3/Program.cs
@@ -9,51 +9,8 @@ using Newtonsoft.Json;
 
 namespace Lesson_2_3
 {
-    public class BankAccount
-    {
-        //public static Dictionary<long, BankAccount> Accounts = new Dictionary<long, BankAccount>();
-        private long _Id;
-        private decimal _Balance;
-        private string _Type, _User_Name;
-        
-
-        public BankAccount(int Balance, string Type, string User_Name)
-        {
-            _Id = Generate_Id.Generate_id();
-            _Balance = Balance;
-            _Type = Type;
-            _User_Name = User_Name;
-            Invoice_Output();
-            
-        }
-        public string Invoice_Output()
-        {
-            return ($"Id = {_Id}\nName = {_User_Name}\nBalance = {_Balance}\nType = {_Type}\n\n");
-        }
-        public bool WithDraw(decimal sum)
-        {
-            _Balance -= sum;
-            return _Balance >= sum;
-                
-        }
-        public bool PutOn(decimal sum)
-        {
-            _Balance += sum;
-            return true;
-        }
-
-        public bool TarnsferMoney(BankAccount Sourse, decimal Ammount)
-        {
-            if (Sourse._Balance >= Ammount)
-            {
-                Sourse._Balance -= Ammount;
-                _Balance += Ammount;
-                return true;
-            }
-            else
-                return false;
-        }
-    }
+    
+    
 
     
 
@@ -87,12 +44,12 @@ namespace Lesson_2_3
             BankAccount account_1 = new BankAccount(1000, "base", "Konstantin");
             BankAccount account_2 = new BankAccount(10000, "base", "Artem");
             Console.WriteLine(account_1.WithDraw(500));
-            Console.WriteLine(account_1.Invoice_Output());
+            Console.WriteLine(account_1);
             Console.WriteLine(account_2.PutOn(500));
-            Console.WriteLine(account_2.Invoice_Output());
+            Console.WriteLine(account_2);
             Console.WriteLine(account_2.TarnsferMoney(account_1, 500));
-            Console.WriteLine(account_1.Invoice_Output());
-            Console.WriteLine(account_2.Invoice_Output());
+            Console.WriteLine(account_1);
+            Console.WriteLine(account_2);
             //Console.ReadKey();
 
 
@@ -103,15 +60,21 @@ namespace Lesson_2_3
 
             Console.WriteLine("Введите путь к файлу");
             string path = Console.ReadLine();
-            
-            using (StreamReader reader = new StreamReader(path))
+            try
             {
-                string line;
-                while ((line = reader.ReadLine()) != null )
+                using (StreamReader reader = new StreamReader(path))
                 {
-                    GB_String.SearchMail(ref line);
-                    Console.WriteLine(line);
+                    string line;
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                        GB_String.SearchMail(ref line);
+                        Console.WriteLine(line);
+                    }
                 }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
             }
             Console.ReadKey();
         }
