76468b82ad503ed0f55eecd106a412c7d569f43f
Константин Потекаев <kostya.potekaev@gmail.com>
2022-07-28 08:13:20 +0300

    Не получается выполнить задачу, так как класс с интерфейсом должен быть нестатическим, а тогда я не понимаю, как мне рабоать с получившимся объектом.

---
+++ Lesson_7/ACoder.cs
@@ -0,0 +1,34 @@
+﻿namespace Lesson_7;
+
+public class ACoder : ICoder
+{
+    public string Encode(string? s)
+    {
+        if (s is null)
+        {
+            throw new ArgumentNullException();
+        }
+        char [] c = s.ToCharArray();
+        for (int i = 0; i < c.Length; i++)
+        {
+            //int a = (char)c[i] + 1;
+            c[i] = (char)((char)c[i] + 1);
+        }
+        s = c.ToString();
+        return s;
+    }
+    public string Decode(string s)
+    {
+        /*if (s is { })
+        {
+            throw new ArgumentNullException("s");
+        }*/
+        return s;
+    }
+
+    public override string ToString()
+    {
+        string s = "";
+        return s;
+    }
+}
---
+++ Lesson_7/Program.cs
@@ -0,0 +1,8 @@
+﻿using Lesson_7; 
+
+string? s = Console.ReadLine();
+var s1 = new ACoder();
+string s2 = s1.Encode(s);
+
+
+Console.WriteLine(s1);
